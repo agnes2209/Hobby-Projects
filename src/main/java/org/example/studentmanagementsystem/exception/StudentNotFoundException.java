b2fddea19972adbf59e6c51f6630ddca3de37429
agnes2209 <agnedavi2209@gmail.com>
2026-02-16 10:41:46 +0530

    Removed Student Management System files from root

--- src/main/java/org/example/studentmanagementsystem/exception/StudentNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package org.example.studentmanagementsystem.exception;
-
-
-public class StudentNotFoundException extends RuntimeException {
-    public StudentNotFoundException(String message) {
-        super(message);
-    }
-}
-
